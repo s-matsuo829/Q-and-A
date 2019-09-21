517fae463ff87ae3568174c64daa83e6742ded7e
s-matsu829 <fakefur829@gmail.com>
2019-09-22 01:35:21 +0900

    Update _messages.blade.php

--- resources/views/layouts/_messages.blade.php
+++ resources/views/layouts/_messages.blade.php
@@ -1,6 +1,6 @@
 @if (session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>成功!</strong> {{ session('success') }}
+        <strong>OK!</strong> {{ session('success') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
