ca21f049f9d71d999828fc031feaaeea8e07290f
s-matsu829 <fakefur829@gmail.com>
2019-09-22 00:53:51 +0900

    other_edit
    ・route('/')にindexを割り当てた
    ・質問の回答を票数順に並び替えた
    ・何も回答が投稿されていない時の見た目をスッキリさせた
    ・何も質問が投稿できていない時にメッセージを表示されるようにした

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,50 +1,52 @@
-<div class="row mt-4">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-title">
-                    <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
-                </div>
-                <hr>
-                @include('layouts._messages')
+@if ($answersCount >0)
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
+                    </div>
+                    <hr>
+                    @include('layouts._messages')
 
-                @foreach ($answers as $answer)
-                    <div class="media">
-                        <div class="d-fex flex-column vote-controls">
-                            @include ('shared._vote', [
-                                'model' => $answer
-                                ])
-                        </div>
-                        <div class="media-body">
-                            {!! $answer->body_html !!}
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="ml-auto">
-                                        @can ('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can ('delete', $answer)
-                                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('本当に削除しますか？')">Delete</button>
-                                            </form>
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    @include ('shared._author', [
-                                        'model' => $answer,
-                                        'label' => 'answered'
+                    @foreach ($answers as $answer)
+                        <div class="media">
+                            <div class="d-fex flex-column vote-controls">
+                                @include ('shared._vote', [
+                                    'model' => $answer
                                     ])
+                            </div>
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="ml-auto">
+                                            @can ('update', $answer)
+                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endcan
+                                            @can ('delete', $answer)
+                                                <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('本当に削除しますか？')">Delete</button>
+                                                </form>
+                                            @endcan
+                                        </div>
+                                    </div>
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include ('shared._author', [
+                                            'model' => $answer,
+                                            'label' => 'answered'
+                                        ])
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <hr>
-                @endforeach
-            </div>
+                        <hr>
+                    @endforeach
+                </div>
+            </div>  
         </div>  
-    </div>  
-</div>  
+    </div> 
+@endif
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,13 +12,12 @@
                             <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
                     </div>
-                  
                 </div>
 
                 <div class="card-body">
                     @include('layouts._messages')
 
-                    @foreach ($questions as $question)
+                    @forelse ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
@@ -57,7 +56,11 @@
                             </div>
                         </div>
                         <hr>
-                    @endforeach
+                    @empty
+                        <div class="alert alert-warning">
+                            <strong>申し訳ございませんm(__)m</strong><br>質問が何も投稿されておりません
+                        </div>
+                    @endforelse
 
                     <div class="mx-auto">
                         {{ $questions->links() }}
