e0daf6ba2e5606f44877ea3a45eb8ff9f835d1f4
s-matsu829 <fakefur829@gmail.com>
2019-09-22 14:07:42 +0900

    add_branch_of_production
    本番とローカルとでセキュアかそうじゃないかを分けた

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
+        else {
+            \URL::forceScheme('http');
+        }
     }
 }
